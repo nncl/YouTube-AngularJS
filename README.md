dac0cc991042286bad89f77a5fd7638f6018b6ef
caue <caue@dindigital.com>
2015-09-21 10:44:42 -0300

    Fixing thesrc call

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # YouTube-AngularJS
 
-Videos from youtube account with AngularJS
+Videos from youtube account with AngularJS + jQuery function later.
+
+## Note
+  The jQuery call is just something that I wanted to test.
--- js/controllers/AppCtrl.js
+++ js/controllers/AppCtrl.js
@@ -1,21 +1,21 @@
 angular.module("myApp", [])
 
-  .controller("AppCtrl", function($scope, $interval, youtube) {
+  .controller("AppCtrl", function($scope, $sce, youtube) {
 
     $scope.getVideos = function() {
       youtube.fetchVideos(function(data) {
-
         $scope.src = '';
         $scope.videos = [];
 
         var length = data.items.length;
 
         for( var i = 0; i < length; i++ ) {
-          $scope.src = 'https://www.youtube.com/embed/' + data.items[i].id.videoId;
+          $scope.src = $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + data.items[i].id.videoId);
           $scope.videos.push( $scope.src );
         }
       });
     };
 
     $scope.getVideos();
+
   });
